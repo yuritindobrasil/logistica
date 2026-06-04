0be77d6ffe831928ba8b6329bb0bdd58475f8142
Yuri <you@example.com>
2026-06-04 16:38:14 -0300

    Upload duplo para forçar sincronização

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-// Sincronização forçada Lovable (Upload Total)
+// Sincronização forçada Lovable (Upload Duplo)
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
